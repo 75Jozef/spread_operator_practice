69659f94a0e328e7771901204e712e7c025118f6
75Jozef <64250704+75Jozef@users.noreply.github.com>
2020-06-27 11:58:15 +0200

    spread_map_to_do_list jednoduché riešenie ako renderovať list cez li položky zoznamu a ukladať do Array cez useState; updatovať...

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,20 +1,43 @@
-import React from "react";
+import React, { useState } from "react";
 
 function App() {
+  const [text, updateText] = useState("");
+  const [array, updateArray] = useState([]); // state môže byť aj pole aj objekt; v tomto prípade si spravím pole;
+
+  function handleChange(event) {
+    const input = event.target.value;
+    updateText(input);
+  }
+
+  function addItem() {
+    updateArray(prevItems => {
+      return [...prevItems, text]; //zavolám si funkciu na update state a vrátim mu cez return novú hodnotui; teraz predošlé a ku nim ako nová položka v poli vznikne text uložený v text; dá sa uložiť aj bez previous items, ale toto je ES6 funkcionalita, takže je to takto easy ;)
+    });
+    updateText(""); //aby sa vymazal text po zadaní do listu
+  }
   return (
     <div className="container">
       <div className="heading">
         <h1>To-Do List</h1>
       </div>
       <div className="form">
-        <input type="text" />
-        <button>
+        <input
+          onChange={handleChange}
+          name="item"
+          type="text"
+          autoComplete="off"
+          value={text}
+        />
+        <button onClick={addItem}>
           <span>Add</span>
         </button>
       </div>
       <div>
         <ul>
-          <li>A Item </li>
+          {array.map(item => (
+            // tak úplne som zabudol na pole.map funkcionalitu reactu a to som teda trubka; takto ľahko môže renderovať akékoľvek pole ako li položky zoznamu;
+            <li>{item}</li>
+          ))}
         </ul>
       </div>
     </div>
